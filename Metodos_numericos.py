003fc9e393eb922cdad2c1ea768e7d22c13adf13
AgustinZavalaA <1930120@upv.edu.mx>
2021-08-04 01:57:06 -0500

    mejor ordenado, string usuario

--- Metodos_numericos.py
+++ Metodos_numericos.py
@@ -8,29 +8,37 @@ from math import exp
 import matplotlib.pyplot as plt
 import numpy as np
 
-    
-def f(t,y):
-    func=t*exp(3*t)-2*y
-    return func
 
-def RK4(t0,y0,h,n):
-    t=np.zeros(n+1)
-    y=np.zeros(n+1)
-    t[0]=t0
-    y[0]=y0
-    print('y(',t,')=',y)
-    
+def f(string, t, y):
+    return eval(string)
+
+
+def RK4(string, t0, y0, h, n):
+    t = np.zeros(n + 1)
+    y = np.zeros(n + 1)
+    t[0] = t0
+    y[0] = y0
+    print("y(", t, ")=", y)
+
     for k in range(n):
-        k1=f(t[k],y[k])
-        k2=f(t[k]+h/2,y[k]+(h/2)*k1)
-        k3=f(t[k]+h/2,y[k]+(h/2)*k2)
-        k4=f(t[k]+h,y[k]+h*k3)
-        y[k+1]=y[k]+(h/6)*(k1+2*k2+2*k3+k4)
-        t[k+1]=t[k]+h
-        print('y(',round(t[k+1],3),')=',y[k+1])
-        
-    plt.plot(t,y)
+        k1 = f(string, t[k], y[k])
+        k2 = f(string, t[k] + h / 2, y[k] + (h / 2) * k1)
+        k3 = f(string, t[k] + h / 2, y[k] + (h / 2) * k2)
+        k4 = f(string, t[k] + h, y[k] + h * k3)
+        y[k + 1] = y[k] + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+        t[k + 1] = t[k] + h
+        print("y(", round(t[k + 1], 3), ")=", y[k + 1])
 
-val=exp(3)/5-exp(3)/25+51*exp(-2)/25
-RK4(0,2,0.01,100)
-print(val)
+    plt.plot(t, y)
+    return y[-1]
+
+
+def main():
+    # val = exp(3) / 5 - exp(3) / 25 + 51 * exp(-2) / 25
+    string_user = "t * exp(3 * t) - 2 * y"
+    val = RK4(string_user, 0, 2, 0.01, 100)
+    print(val)
+
+
+if __name__ == "__main__":
+    main()
